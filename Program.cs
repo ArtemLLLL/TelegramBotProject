e09ca8fc90ff12b3d03c814610617dcb11bc61c2
Artem Liadusov <104716729+ArtemLLLL@users.noreply.github.com>
2023-01-25 23:21:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using System.Configuration;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+
+namespace TelegramBotProject
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string value = ConfigurationManager.AppSetting["key"];
+            var client = new TelegramBotClient($"{value}");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                Console.WriteLine($"{message.Chat.FirstName} | {message.Text} | {message.Photo}");
+
+                if (message.Text.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать в бот повторюша");
+                    return;
+                }
+                else
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, message.Text);
+                    return;
+                }
+            }
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
